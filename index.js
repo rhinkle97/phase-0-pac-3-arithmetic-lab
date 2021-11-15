33eba6260f8be84c13a079ec5c94c9a954807ab9
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-11-15 15:10:18 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+number = 10;
+
+function add(number) {
+    return(number += 5);
+}
+divideBy(3);
+
+function multiply(number) {
+    return(number * 10);
+}
+divideBy(5);
+
+function subtract(number) {
+    return(number -= 5);
+}
+divideBy(5);
+
+function divide(number) {
+    return(number / 2);
+}
+divide(5);
+
+a = 5
+b = 10
+function add(a, b) {
+    return(a + b);
+}
+function subtract(a, b) {
+    return(a - b);
+}
+function multiply(a, b) {
+    return(a * b);
+}
+function divide(a, b) {
+    return(a/b);
+}
